b13f3c9d0709bb1fd965731dabe8c8ac2b5a9f6d
Jo yerin <yerin65@naver.com>
2022-09-14 02:17:32 +0900

    카메라 구현

---
+++ app/src/main/java/com/example/hanium2/RegisterCompleteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hanium2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterCompleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_complete)
+    }
+
+}
---
+++ app/src/main/java/com/example/hanium2/dialog_select_img.kt
@@ -0,0 +1,11 @@
+package com.example.hanium2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dialog_select_img : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dialog_select_img)
+    }
+}
